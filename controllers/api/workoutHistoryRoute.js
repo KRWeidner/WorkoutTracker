fadbdb5dbd8b825eb992200c7cac84d15f0c1f66
KatyW <ktabe13@gmail.com>
2023-08-15 20:07:55 -0500

    homepage updates

--- controllers/api/workoutHistoryRoute.js
+++ controllers/api/workoutHistoryRoute.js
@@ -19,4 +19,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const newUserHistoryData = await UserWkOuts.create({
+            ...req.body,
+            user_id: req.session.user_id,
+          });
+
+        const pastWorkouts = userHistoryData.map((history) => history.get({ plain: true }))
+
+        res.status(200).json(newUserHistoryData);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
---
+++ public/js/userHistory.js
@@ -0,0 +1,29 @@
+const { DATE } = require("sequelize");
+
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the new post form
+    const title = document.querySelector('#title-new-post').value.trim();
+    const content = document.querySelector('#content-new-post').value.trim();
+  
+    if (title && content) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/workoutHistory', {
+        method: 'POST',
+        body: JSON.stringify({ userId: title, routineId: content, date: DATE.now }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the home page
+        document.location.replace('/workoutHistory');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('#createPost')
+    .addEventListener('click', newPostFormHandler);
