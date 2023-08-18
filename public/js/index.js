f76178012ab10cc5dd356f0a9c50d47354ffc9c2
Chue Vang <chuyieng@gmail.com>
2023-08-17 20:57:48 -0500

    buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,9 @@
 const routDescription = document.getElementById('routDesc');
 const routName = document.getElementById('routName');
+const routTitle = document.getElementById('routTitle');
+const exercList = document.getElementById('exercList');
+const exercDate = document.getElementById('exercDate');
+const wkOutHistContainer = document.getElementById('wkOutHistContainer');
 
 function showDescription() {
 
@@ -15,5 +19,13 @@ function closeDescription() {
 
 };
 
+function enlargeBox() {
+
+    routTitle.style.display = "none";
+    exercList.style.display = "block";
+
+}
+
 routName.addEventListener("mouseenter", showDescription);
 routDescription.addEventListener("mouseleave", closeDescription);
+wkOutHistContainer.addEventListener("mouseenter", enlargeBox);
